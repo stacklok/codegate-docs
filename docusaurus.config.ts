1bb77c13f0ab13cfcc302ad6f67ac2668904a820
Dan Barr <dan@stacklok.com>
2024-12-16 20:32:05 -0500

    Add Google Analytics tracking ID

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -51,6 +51,10 @@ const config: Config = {
         theme: {
           customCss: './src/css/custom.css',
         },
+        gtag: {
+          trackingID: 'G-WYFTM4LMCM',
+          anonymizeIP: true,
+        },
       } satisfies Preset.Options,
     ],
   ],
