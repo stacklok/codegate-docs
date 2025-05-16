7d297b67cf7272ffb2fe4d648b7d057410125ebe
Dan Barr <dan@stacklok.com>
2025-05-16 16:04:43 -0400

    Update footer and styles

--- docusaurus.config.ts
+++ docusaurus.config.ts
@@ -69,9 +69,9 @@ const config: Config = {
 
   themeConfig: {
     colorMode: {
-      defaultMode: 'light',
-      disableSwitch: true,
-      respectPrefersColorScheme: false,
+      defaultMode: 'dark',
+      disableSwitch: false,
+      respectPrefersColorScheme: true,
     },
     // Replace with your project's social card
     // image: 'img/docusaurus-social-card.jpg',
@@ -98,80 +98,66 @@ const config: Config = {
         },
         {
           href: 'https://github.com/stacklok',
-          className: 'header-github-link',
+          className: 'github-link',
           position: 'right',
           'aria-label': 'GitHub',
         },
         {
           href: 'https://discord.gg/stacklok',
-          className: 'header-discord-link',
+          className: 'discord-link',
           position: 'right',
           'aria-label': 'Discord',
         },
         {
           href: 'https://youtube.com/@stacklok',
-          className: 'header-youtube-link',
+          className: 'youtube-link',
           position: 'right',
           'aria-label': 'YouTube',
         },
       ],
     },
     footer: {
-      style: 'light',
+      style: 'dark',
       links: [
         {
           items: [
             {
-              html: `<a href="https://stacklok.com/"><img src="/img/stacklok-wordmark-black.svg" alt="Stacklok Logo" style="margin-top: -16px" /></a>`,
+              html: `<div style="display: flex;">
+                       <a href="https://stacklok.com/"><img src="/img/stacklok-wordmark-white.svg" alt="Stacklok Logo" width="150px" /></a>
+                       <a href="https://github.com/stacklok" target="_blank" class="footer__icon__custom navbar__link github-link" style="padding-left: 30px;"></a>
+                       <a href="https://discord.gg/stacklok" target="_blank" class="footer__icon__custom navbar__link discord-link"></a>
+                       <a href="https://youtube.com/@stacklok" target="_blank" class="footer__icon__custom navbar__link youtube-link"></a>
+                     </div>`,
             },
             {
               html: `We build solutions that help developers create more safe and sustainable code.`,
             },
+          ],
+        },
+        {
+          title: 'Projects',
+          items: [
             {
-              html: `<div style="display: flex; padding: 20px 0px;">
-                       <a href="https://youtube.com/@stacklok" target="_blank" class="footer__icon__custom navbar__link header-youtube-link" style="padding-left: 0px;"></a>
-                       <a href="https://github.com/stacklok" target="_blank" class="footer__icon__custom navbar__link header-github-link"></a>
-                       <a href="https://discord.gg/stacklok" target="_blank" class="footer__icon__custom navbar__link header-discord-link"></a>
-                     </div>`,
+              html: '<a href="https://github.com/stacklok" target="_blank" class="footer__link-item github-link">&nbsp;&nbsp;CodeGate</a>',
+            },
+            {
+              html: '<a href="https://github.com/stacklok" target="_blank" class="footer__link-item github-link">&nbsp;&nbsp;ToolHive</a>',
             },
           ],
         },
         {
-          title: 'Links',
+          title: 'Community',
           items: [
             {
-              html: '<a href="https://www.stacklok.com" target="_blank" class="navbar__link header-stacklok-link"">&nbsp;&nbsp;Stacklok</a>',
+              html: '<a href="https://discord.gg/stacklok" target="_blank" class="footer__link-item discord-link">&nbsp;&nbsp;Discord</a>',
             },
             {
-              html: '<a href="https://youtube.com/@stacklok" target="_blank" class="navbar__link header-youtube-link"">&nbsp;&nbsp;YouTube</a>',
-            },
-            {
-              html: '<a href="https://github.com/stacklok" target="_blank" class="navbar__link header-github-link">&nbsp;&nbsp;GitHub</a>',
-            },
-            {
-              html: '<a href="https://discord.gg/stacklok" target="_blank" class="navbar__link header-discord-link">&nbsp;&nbsp;Discord</a>',
-            },
-          ],
-        },
-        {
-          title: 'Support',
-          items: [
-            {
-              label: 'Documentation',
-              to: '/',
-            },
-            {
-              label: 'Get Started',
-              to: '/quickstart',
+              html: '<a href="https://youtube.com/@stacklok" target="_blank" class="footer__link-item youtube-link"">&nbsp;&nbsp;YouTube</a>',
             },
           ],
         },
       ],
-      copyright: `<hr style="margin: 24px 0px 40px;">
-                  <div style="display: flex;">
-                    <div>Copyright © ${new Date().getFullYear()} Stacklok, All Rights Reserved</div>
-                    <div style="margin-left: auto; text-transform: uppercase; font-size: 14px; font-weight: 600;">Powered by <img src="/img/stacklok-wordmark-black.svg" height="20px" style="margin-left: 1rem;" /></div>
-                  </div>`,
+      copyright: `Copyright © ${new Date().getFullYear()} Stacklok, Inc.`,
     },
     prism: {
       theme: prismThemes.github,
@@ -183,6 +169,23 @@ const config: Config = {
       options: {
         themeVariables: {
           fontFamily: 'Figtree, system-ui, sans-serif',
+          primaryColor: '#E31C79',
+          primaryBorderColor: '#E31C79',
+          lineColor: '#E31C79',
+          secondaryColor: '#F3E6EE',
+          tertiaryColor: '#FAF0F6',
+          mainBkg: '#E31C79',
+          noteBkgColor: '#444444',
+          noteTextColor: '#FFFFFF',
+          // BEGIN sequenceDiagram styles
+          actorBorder: '#E31C79',
+          actorLineColor: '#E31C79',
+          actorTextColor: '#FFFFFF',
+          activationBkgColor: '#E31C79',
+          activationBorderColor: '#E31C79',
+          labelBoxBkgColor: '#444444',
+          labelTextColor: '#FFFFFF',
+          // END sequenceDiagram styles
         },
       },
     },
