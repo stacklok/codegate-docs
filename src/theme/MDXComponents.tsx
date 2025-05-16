66b757197d5b9cc8217ebea82212b3bfbfd099c8
Dan Barr <dan@stacklok.com>
2025-05-16 17:27:01 -0400

    Remove columns components from global scope

--- src/theme/MDXComponents.tsx
+++ src/theme/MDXComponents.tsx
@@ -7,12 +7,8 @@
 */
 
 import MDXComponents from '@theme-original/MDXComponents';
-import Columns from '@site/src/components/Columns';
-import Column from '@site/src/components/Column';
 
 export default {
   // Reusing the default mapping
   ...MDXComponents,
-  Columns,
-  Column,
 };
